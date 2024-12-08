677ec9cc3ae44b8f5c10ae3509bec50690cd5b77
Maciej Ładoś <maciej@lados.dev>
2024-12-08 22:44:44 +0100

    Setup workspaces for e2e tests

---
+++ packages/adonisjsx-e2e/config/default_layout.tsx
@@ -0,0 +1,12 @@
+export function DefaultLayout({ children }: PropsWithChildren) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>AdonisJS</title>
+      </head>
+      <body>{children}</body>
+    </html>
+  )
+}
---
+++ packages/adonisjsx-e2e/config/jsx.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'adonisjsx'
+import { DefaultLayout } from './default_layout.js'
+
+const jsxConfig = defineConfig({
+  defaultLayout: DefaultLayout,
+})
+
+export default jsxConfig
